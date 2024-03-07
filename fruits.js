4c2ca1e3b7b0251f6049fb71da36899a1852c927
Tyler <73407076+harukasu111@users.noreply.github.com>
2024-03-07 11:31:47 -0500

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,49 @@
+const FRUITS_BASE = [
+  {
+    name: "./public/base/00_cherry.png",
+    radius: 33 / 2,
+  },
+  {
+    name: "./public/base/01_strawberry",
+    radius: 48 / 2,
+  },
+  {
+    name: "./public/base/02_grape",
+    radius: 61 / 2,
+  },
+  {
+    name: "./public/base/03_gyool",
+    radius: 69 / 2,
+  },
+  {
+    name: "./public/base/04_orange",
+    radius: 89 / 2,
+  },
+  {
+    name: "./public/base/05_apple",
+    radius: 114 / 2,
+  },
+  {
+    name: "./public/base/06_pear",
+    radius: 129 / 2,
+  },
+  {
+    name: "./public/base/07_peach",
+    radius: 156 / 2,
+  },
+  {
+    name: "./public/base/08_pineapple",
+    radius: 177 / 2,
+  },
+  {
+    name: "./public/base/09_melon",
+    radius: 220 / 2,
+  },
+  {
+    name: "./public/base/10_watermelon",
+    radius: 259 / 2,
+  },
+];
+
+
+export { FRUITS_BASE };
